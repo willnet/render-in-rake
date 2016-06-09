ce760e0f49e8a9f14af31c871c9716dcdf1bc5b0
willnet <netwillnet@gmail.com>
2016-06-09 13:50:30 +0900

    Enable see pdf

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,2 +1,10 @@
 class ThingsController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: 'things'
+      end
+    end
+  end
 end
